b99dacaeaaa482cffb78a093371a287dc8348df3
Samuel <samuel.olaya@correo.unimet.edu.ve>
2025-06-01 21:41:44 -0400

    Crear y editar datos del usuario para firestore

--- lib/services/edit_player_service.dart
+++ lib/services/edit_player_service.dart
@@ -3,12 +3,9 @@ import '../features/auth/models/user_model.dart';
 
 class edit_player {
 
-  
+    final CollectionReference players = FirebaseFirestore.instance.collection('players');
 
-
-
-
-
-
-
+    Future<void> createplayerinfo(UserModel user) async {
+    await players.doc(user.uid).set(user.toMap());
+  }
 }
